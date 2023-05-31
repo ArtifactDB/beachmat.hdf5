570125bf4624d2285ce90a075f5278d342f25449
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-05-31 11:30:58 -0700

    Support in-memory caching of the loaded matrix.

--- R/initializeCpp.R
+++ R/initializeCpp.R
@@ -4,6 +4,8 @@
 #'
 #' @param x A \pkg{HDF5Array} seed object.
 #' @param ... Further arguments, ignored.
+#' @param memorize Logical scalar specifying whether to load the matrix data in \code{x} into memory, if it has not already been loaded.
+#' See \code{\link{checkMemoryCache}} for details.
 #'
 #' @return An external pointer that can be used in any \pkg{tatami}-compatible function.
 #'
@@ -24,14 +26,24 @@ NULL
 #' @rdname initializeCpp
 #' @import methods
 #' @importFrom beachmat initializeCpp
+#' @importFrom beachmat checkMemoryCache
 #' @importFrom HDF5Array H5SparseMatrixSeed
-setMethod("initializeCpp", "H5SparseMatrixSeed", function(x, ...) {
-    initialize_from_hdf5_sparse(x@filepath, x@group, nrow(x), ncol(x), is(x, "CSR_H5SparseMatrixSeed"))
+setMethod("initializeCpp", "H5SparseMatrixSeed", function(x, ..., memorize = FALSE) {
+    if (memorize) {
+        checkMemoryCache(paste("hdf5-sparse", x@filepath, x@name, sep=":"), function() loadIntoMemory(x))
+    } else {
+        initialize_from_hdf5_sparse(x@filepath, x@group, nrow=nrow(x), ncol=ncol(x), byrow=is(x, "CSR_H5SparseMatrixSeed"))
+    }
 })
 
 #' @export
 #' @rdname initializeCpp
 #' @importFrom HDF5Array HDF5ArraySeed
-setMethod("initializeCpp", "HDF5ArraySeed", function(x, ...) {
-    initialize_from_hdf5_dense(x@filepath, x@name)
+#' @importFrom beachmat checkMemoryCache
+setMethod("initializeCpp", "HDF5ArraySeed", function(x, ..., memorize = FALSE) {
+    if (memorize) {
+        checkMemoryCache(paste("hdf5-dense", x@filepath, x@name, sep=":"), function() loadIntoMemory(x))
+    } else {
+        initialize_from_hdf5_dense(x@filepath, x@name)
+    }
 })
